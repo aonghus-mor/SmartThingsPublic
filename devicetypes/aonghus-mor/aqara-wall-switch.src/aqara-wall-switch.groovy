b7583d9d6118e402ae77981a00112545a0f0041a
Angus MacKinnon <angus@mackinnon.me.uk>
2021-12-29 10:22:19 +0000

    Minor correction

--- devicetypes/aonghus-mor/aqara-wall-switch.src/aqara-wall-switch.groovy
+++ devicetypes/aonghus-mor/aqara-wall-switch.src/aqara-wall-switch.groovy
@@ -40,6 +40,10 @@ metadata
                     //ocfDeviceType: "x.com.st.d.remotecontroller"
                     //vid: "1c4f60a8-b69f-37dd-9f1b-235e1d6f54bc",// button (with battery)
                     //ocfDeviceType: "x.com.st.d.remotecontroller" 
+                    //vid: "2a0d4f73-869d-3e3e-93bf-8ef8e45de69d", // decoupled (no neutral)
+                    //ocfDeviceType: "x.com.st.d.remotecontroller"
+                    //vid: "5d9177d1-3af5-30b9-b32e-f2c109aa590b", // decoupled (with neutral)
+                    //ocfDeviceType: "x.com.st.d.remotecontroller"
                 )
     {
         capability "Configuration"
@@ -98,7 +102,14 @@ metadata
                 manufacturer: "LUMI", model: "lumi.switch.l1aeu1", deviceJoinName: "Aqara Switch EU-01" 
         fingerprint profileId: "0104", deviceId: "0100", inClusters: "0000,0002,0003,0004,0005,0006,0009", outClusters: "000A,0019", 
                 manufacturer: "LUMI", model: "lumi.switch.l2aeu1", deviceJoinName: "Aqara Switch EU-02"              
-     }
+    	fingerprint profileId: "0104", deviceId: "0100", inClusters: "0000,0002,0003,0004,0005,0006,0009", outClusters: "000A,0019", 
+                manufacturer: "LUMI", model: "lumi.switch.b1lc04", deviceJoinName: "Aqara Switch QBKG38LM" 
+        fingerprint profileId: "0104", deviceId: "0100", inClusters: "0000,0002,0003,0004,0005,0006,0009", outClusters: "000A,0019", 
+                manufacturer: "LUMI", model: "lumi.switch.b2lc04", deviceJoinName: "Aqara Switch QBKG39LM"      
+        fingerprint profileId: "0104", deviceId: "0100", inClusters: "0000,0002,0003,0004,0005,0006,0009", outClusters: "000A,0019", 
+                manufacturer: "LUMI", model: "lumi.switch.b1nc01", deviceJoinName: "Aqara Switch QBKG40LM" 
+        fingerprint profileId: "0104", deviceId: "0100", inClusters: "0000,0002,0003,0004,0005,0006,0009", outClusters: "000A,0019", 
+                manufacturer: "LUMI", model: "lumi.switch.b2nc01", deviceJoinName: "Aqara Switch QBKG41LM"        }
 	
     preferences 
     {	
@@ -230,9 +241,9 @@ private def parseCatchAllMessage(String description)
         	break
         case 0x0006: 	
 			//if ( state.oldOnOff )
-            	events = events + parseSwitchOnOff( [endpoint:cluster.sourceEndpoint.toString(), value: cluster.data[0].toString()] )
+            	//events = events + parseSwitchOnOff( [endpoint:cluster.sourceEndpoint.toString(), value: cluster.data[0].toString()] )
             //else
-            //	displayDebugLog('CatchAll message ignored!')
+            	displayDebugLog('CatchAll message ignored!')
             break
     }
     return events
@@ -647,17 +658,14 @@ def childRefresh(String dni, Map sets)
 def on() 
 {
     displayDebugLog("Switch 1 pressed on")
-    Map button = [name: 'button', value: 'pushed', data:[buttonNumber: 1], isStateChange: true]
-    sendEvent(button)
-    displayDebugLog(button)
-    def cmd = []
-    if ( state.unwiredSwitches[0] )
+    if ( !state.decoupled[0] )
     {
-    	Map swoff = [name: 'switch', value: 'off']
-    	sendEvent( swoff )
-        displayDebugLog(swoff)
+    	Map button = [name: 'button', value: 'pushed', data:[buttonNumber: 1], isStateChange: true]
+    	sendEvent(button)
+    	displayDebugLog(button)
     }
-    else
+    def cmd = []
+    if ( ! state.unwiredSwitches[0] )
     	cmd = zigbee.command(0x0006, 0x01, "", [destEndpoint: state.endpoints[0]] )
     displayDebugLog( cmd )
     return cmd 
@@ -666,18 +674,14 @@ def on()
 def off() 
 {
     displayDebugLog("Switch 1 pressed off")
-    Map button = [name: 'button', value: 'pushed', data:[buttonNumber: 1], isStateChange: true]
-    sendEvent( button )
-    displayDebugLog(button)
-    //def cmd = state.unwiredSwitches[0] ? [] : zigbee.command(0x0006, 0x00, "", [destEndpoint: state.endpoints[0]] )
-    def cmd = []
-    if ( state.unwiredSwitches[0] )
+    if ( !state.decoupled[0] )
     {
-    	Map swoff = [name: 'switch', value: 'off']
-    	sendEvent( swoff )
-        displayDebugLog(swoff)
-    }
-    else
+    	Map button = [name: 'button', value: 'pushed', data:[buttonNumber: 1], isStateChange: true]
+    	sendEvent( button )
+    	displayDebugLog(button)
+     }
+    def cmd = []
+    if ( !state.unwiredSwitches[0] )
     	cmd = zigbee.command(0x0006, 0x00, "", [destEndpoint: state.endpoints[0]] )
     
     displayDebugLog(cmd)
@@ -737,7 +741,7 @@ def refresh()
     if ( state.decoupled == null )
     	state.decoupled = []
     if ( settings.decoupled == null )
-    	ssettings.decoupled = false
+    	settings.decoupled = false
     state.decoupled[0] = settings.decoupled
     displayDebugLog("Decoupled: ${state.decoupled}")
     
@@ -1004,10 +1008,19 @@ private getNumButtons()
 			break
         case "lumi.switch.b2laus01": //Lumi WS-USC02
         case "lumi.switch.l2aeu1": //Aqara Switch EU-02
+        case "lumi.switch.b2lc04": //QBKG39LM
         	state.numSwitches = 2
             state.numButtons = 2
             //state.endpoints = [0x01,0x02,0xF3,0x05,0x06,0xF5,0xF6]
             state.endpoints = [0x01,0x02,0xF3,0x29,0x2a,0xF5,0xF6, 0x33,0x34]
+			state.hasFCC0 = true
+			break
+        case "lumi.switch.b1lc04": //QBKG38LM
+        case "lumi.switch.b1nc01": //QBKG40LM
+        case "lumi.switch.b2nc01": //QBKG41LM
+        	state.numSwitches = 1
+            state.numButtons = 1
+            state.endpoints = [0x01,0xF02,0xF3,0x29,0xF4,0xF5,0xF6, 0x33,0xF7]
 			state.hasFCC0 = true
 			break
         case "lumi.remote.b486opcn01":
